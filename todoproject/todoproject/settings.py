e415a6f02a355a7ab9eb0e6a78fbd4660497b41f
AHNDOIL <ahj3579@naver.com>
2022-05-22 22:54:38 +0900

    staticfiles_dirs modify

--- todoproject/todoproject/settings.py
+++ todoproject/todoproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'todolist',
     'users',
+   
 ]
 
 MIDDLEWARE = [
@@ -117,9 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-STATICFIELS_DIRS = [
+STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 # Default primary key field type
